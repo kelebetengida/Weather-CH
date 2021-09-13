1128e4c49f584a240726d8e23715349477a1f770
Kelebet Engida <engida398@gmail.com>
2021-09-13 00:42:26 -0700

    Updated the icon api and render it to the html page.

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
       })
       .then(function (data) {
           console.log(data);
-          let card= $("<div>").addClass("card")
-          let name=$("<h3>").addClass("card-text").text(`${data.name} (${new Date().toLocaleDateString()})`);
+          let card= $("<div class='card shadow-lg text-white bg-primary'>").addClass("card")
+          let name=$("<h1>").addClass("card-text").text(`${data.name} (${new Date().toLocaleDateString()})`);
           const icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
 
           let temp=$("<p>").addClass("card-text").text(`Temp: ${data.main.temp}`)
@@ -24,7 +24,9 @@ $(document).ready(function () {
           const lat=data.coord.lat;
           const lon=data.coord.lon;
           searchUv(lat, lon);
+          fiveDay(city);
       })
+      
       .catch(function(err){
         console.err(err);
       })
@@ -38,46 +40,87 @@ $(document).ready(function () {
         .then(function(data){
           let uvi=$("<p>").addClass("card-text").text(`UV Index: ${data.current.uvi}`)
           $(".subtitle .card").append(uvi)
+         
+          
         })
   }
   
 
   function fiveDay(city){
-    let qUrl=`api.openweathermap.org/data/2.5/forecast?q=${city}&appid=7de72314249ebce4535755d73b07b7fa`
+    let url=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=7de72314249ebce4535755d73b07b7fa`
     fetch(url)
         .then(function(response){
           return response.json();
         })
         .then(function(data){
-          for(i=1;i<6;i++){
+          
+          for (let i = 0; i < data.list.length; i += 8) {
+
+            let fiveForcast = $("<div class='card shadow-lg text-white bg-primary mx-auto  h3' style='width: 15rem; height: 20rem;'>");
+            let date = data.list[i].dt_txt;
+            let setDate = date.substr(0,10)
+            let temp = data.list[i].main.temp;
+            let winds = data.list[i].wind.speed;
+            let humd = data.list[i].main.humidity;
+            let weather = data.list[i].weather[0].main
             
+
+            if (weather === "Rain") {
+                var icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+                icon.attr("style", "height: 40px; width: 40px");
+            } else if (weather === "Clouds") {
+                var icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+                icon.attr("style", "height: 40px; width: 40px");
+            } 
+             else if (weather === "Clear") {
+                var icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+                icon.attr("style", "height: 40px; width: 40px");
+            }
+             else if (weather === "Drizzle") {
+                var icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+                icon.attr("style", "height: 40px; width: 40px");
+            }
+             else if (weather === "Snow") {
+                var icon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+                icon.attr("style", "height: 50px; width: 40px");
+            }
+
+            let fiveDayDate = $("<h2 class='card-title'>").text(setDate);
+            let Temp = $("<p class='card-text'>").text("Temp: " + temp);
+            let windSpeed = $("<p class='card-text'>").text("Wind: " + winds);
+            let humid = $("<p class='card-text'>").text("Humidity: " + humd);
+            
+            
+          
+
+            fiveForcast.append(fiveDayDate);
+            fiveForcast.append(icon);
+            fiveForcast.append(Temp);
+            fiveForcast.append(windSpeed);
+            fiveForcast.append(humid);
+            
+           
+            $("#city-container").append(fiveForcast);
           }
         })
 
     }
     $("#search-btn").on("click", function(e){
       e.preventDefault();
-      let cityName=$("#cityname").val();
-      
+      let cityName=$("#cityname").val().trim();
       searchweather(cityName);
+      var arrayS=[];
+      arrayS.push(cityName);
+      localStorage.setItem("cityName", JSON.stringify(arrayS))
+      searchJistory ();
       
     })
-  // var arraySearch = [];
-  // function showHistory() {
-  //   if (JSON.parse(localStorage.getItem("#btn")) !== null) {
-  //     arraySearch = JSON.parse(localStorage.getItem("#btn"))
-  //     $("#btn").empty();
-  //     $("#btn").append();
-  //     for (let i = 0; i < arraySearch.length; i++) {
-  //       $("#btn").append(`<section id="search">${arraySearch[i]}</section>`)
-  //     }
-  //   }
+    function searchJistory () {
+      var lastSearch = JSON.parse(localStorage.getItem("cityName"));
+      var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(lastSearch);
+      $("#cards").append(searchDiv);
+  }
+  
+ 
 
-  // }
-  // function pageIntiaize() {
-  //   if (JSON.parse(localStorage.getItem("#btn")) !== null) {
-  //     showHistory()
-  //   }
-  // }
-  // searchweather("Portland");
 });
